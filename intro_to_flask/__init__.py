95f104e8c8b4aeefad85927191956b086996ffa8
atindale <alan@flexiblesoftware.com.au>
2015-02-12 21:58:04 +1000

    Moved configuration settings to separate file.

--- intro_to_flask/__init__.py
+++ intro_to_flask/__init__.py
@@ -2,18 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.secret_key = 'development key'
- 
-app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 465
-app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = 'alan@flexiblesoftware.com.au'
-app.config["MAIL_PASSWORD"] = 'M0pSp0ng3'
+app.config.from_object('config')
 
 from routes import mail
 mail.init_app(app)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:aerielm1@127.0.0.1/flexible_software"
 
 from models import db
 db.init_app(app)
